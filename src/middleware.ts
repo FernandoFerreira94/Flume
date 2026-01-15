02580e02988abfbf30a6fefdb59baf438f176b8d
Fernando <fernandoeqp59@gmail.com>
2026-01-15 15:02:43 -0300

    ok

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,10 +9,13 @@ export async function middleware(request: NextRequest) {
   } = await supabase.auth.getSession();
   const isPrivateRoute = request.nextUrl.pathname.startsWith("/dashboard");
 
+  /*
   if (isPrivateRoute && !session) {
     const loginUrl = new URL("/", request.url);
     return NextResponse.redirect(loginUrl);
   }
+
+  */
 
   return response;
 }
