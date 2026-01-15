2be7ca87d67c3732cfe9c8c16915b7c2455f8cb3
Fernando <fernandoeqp59@gmail.com>
2026-01-14 22:01:33 -0300

    ok

--- src/service/auth/loginWithGoogleService.ts
+++ src/service/auth/loginWithGoogleService.ts
@@ -3,6 +3,9 @@ import { supabaseBrowser } from "@/lib/supabase/client";
 export async function loginWithGoogleService() {
   const { data, error } = await supabaseBrowser.auth.signInWithOAuth({
     provider: "google",
+    options: {
+      redirectTo: `https://flume-sigma.vercel.app/**`,
+    },
   });
 
   if (error) {
